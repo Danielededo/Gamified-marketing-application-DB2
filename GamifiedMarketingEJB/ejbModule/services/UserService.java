57c41b104da382d5b29b262d4781cb972a6653f5
ManfrediComella <manfredi.comella@mail.polimi.it>
2021-08-18 20:22:12 +0000

    Login

---
+++ GamifiedMarketingEJB/ejbModule/services/UserService.java
@@ -0,0 +1,38 @@
+package services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+import entities.User;
+import exceptions.CredentialsException;
+
+@Stateless
+public class UserService {
+	
+	@PersistenceContext(unitName = "GamifiedMarketingEJB")
+	private EntityManager em;
+
+	public UserService() {
+	}
+	
+	public User checkCredentials(String usrn, String pwd) throws CredentialsException, NonUniqueResultException {
+		List<User> uList = null;
+		try {
+			uList = em.createNamedQuery("User.checkCredentials", User.class).setParameter(1, usrn).setParameter(2, pwd)
+					.getResultList();
+		} catch (PersistenceException e) {
+			throw new CredentialsException("Could not verify credentals");
+		}
+		if (uList.isEmpty())
+			return null;
+		else if (uList.size() == 1)
+			return uList.get(0);
+		throw new NonUniqueResultException("More than one user registered with same credentials");
+
+	}
+}
---
+++ GamifiedMarketingWEB/src/controller/GoToLogin.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import entities.User;
+
+@WebServlet("/LoginPage")
+public class GoToLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	
+	public GoToLogin() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {	
+		// Redirect to the login page
+		String path = "/index.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void destroy() {
+	}
+}
